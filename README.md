24b25d68a3ebbb6a5df47c1d34544c601a128a8c
Colin Shaw <colin.shaw@aya.yale.edu>
2018-01-07 16:56:17 -0600

    Major changes to support being able to have a null class

--- README.md
+++ README.md
@@ -5,19 +5,25 @@ TensorFlow, scikit-learn and OpenCV.  OpenCV is used for
 actual image matching using a Haar cascade to detect 
 face regions.  These are fed into VGG16 with the 
 classification layers removed.  The resulting flattened 
-convolutional vector is fed into a K Nearest Neighbor
-classifier to obtain the identification.  
+convolutional vector has principal component analysis
+applied, the result of which is fed to a linear support
+vector machine classifier to obtain identification.
 
 What you do is drop some training data in `/data/train` 
 using a common name with numeric suffix for the repeats 
 of a given class.  These will all be mapped to the 
-non-numeric part of the file name as a label.  The number of
-nearest neighbors is based on the number of unique labels.  When 
-the app first runs, it will notice a lack of the model and 
-build one from the files present.  Next time it runs it
-will simply use the existing classifier.  The classifier
-is stored in the `/models` directory.  Just delete it if
-you want to refresh the classifier model.
+non-numeric part of the file name as a label.  You can feed
+a generic catch-all class like `none` for a wide range of
+images that do not match a particular class.  The reason for
+doing the PCA before the classifier is to reduce the
+dimensionality of the VGG16 vector to something where 
+classification works against the `none` null class. When 
+the app first runs, it will notice a lack of the pre-computed
+model and build one from the files present.  Next time it 
+runs it will simply use the existing classifier.  Both the
+classifier and decomposition model are stored in the `/models` 
+directory.  Just delete it if you want to refresh the 
+classifier model with new data.
 
 To perform a match, you just send an image by `POST` to
 the service.  Here are some examples based on the data that
@@ -26,7 +32,12 @@ this comes with:
 ```
 curl -X POST -F 'image=@data/test/norton.jpg' http://localhost:5000/match
 curl -X POST -F 'image=@data/test/hopkins.jpg' http://localhost:5000/match
+curl -X POST -F 'image=@data/test/cruise.jpg' http://localhost:5000/match
+curl -X POST -F 'image=@data/test/roberts.jpg' http://localhost:5000/match
 ```
 
-Have fun!
+Given the training data, the Edward Norton and Anthony Hopkins matches
+should return the proper class, whereas Tom Cruise and Julia Roberts
+should return the `none` class.
+
 
--- src/image_match.py
+++ src/image_match.py
@@ -1,16 +1,18 @@
-from keras.applications                import VGG16
-from keras.models                      import Model
-from keras.applications.imagenet_utils import preprocess_input
-from scipy.misc                        import imread, imresize
-from sklearn.neighbors                 import KNeighborsClassifier
+from keras.applications    import VGG16, imagenet_utils
+from keras.models          import Model
+from scipy.misc            import imread, imresize
+from sklearn.svm           import LinearSVC
+from sklearn.decomposition import PCA
+from os.path               import isfile
 import numpy as np
 import pickle
 import cv2
 import os
 
 
-CLASSIFIER_MODEL = 'models/model.p'
-CASCADE_MODEL    = 'models/haarcascade_frontalface_alt.xml'
+CLASSIFIER_MODEL    = 'models/classifier.p'
+DECOMPOSITION_MODEL = 'models/decomposition.p'
+CASCADE_MODEL       = 'models/haarcascade_frontalface_alt.xml'
 
 
 class ImageMatch(object):
@@ -18,10 +20,13 @@ class ImageMatch(object):
     def __init__(self):
         self.__make_vgg16_model()
         self.__cascade = cv2.CascadeClassifier(CASCADE_MODEL)
-        if os.path.isfile(CLASSIFIER_MODEL):
+        if isfile(CLASSIFIER_MODEL) and isfile(DECOMPOSITION_MODEL):
             self.__load_classifier()
+            self.__load_decomposition()
         else:
             self.__train_classifier()
+            self.__save_classifier()
+            self.__save_decomposition()
 
     def __make_vgg16_model(self):
         vgg16 = VGG16(weights='imagenet', include_top=False)
@@ -29,28 +34,6 @@ class ImageMatch(object):
             inputs  = [vgg16.input], 
             outputs = [vgg16.get_layer('block5_pool').output]
         )
-
-    def __vgg16_predict(self, image):
-        image = imresize(image, (224,224)).astype(np.float32)
-        image = preprocess_input(image)
-        image = np.reshape(image, (1,224,224,3))
-        label = self.__model.predict(image)
-        label = label.ravel()
-        return label
-
-    def __generate_training_data(self):
-        print('Training...')
-        features, labels = [], []
-        dirlist = os.listdir('data/train')
-        images  = [f for f in dirlist if 'jpg' in f] 
-        for image in images:
-            filename = os.path.splitext(image)[0]
-            label    = ''.join(i for i in filename if not i.isdigit())
-            labels.append(label)
-            feature = imread('data/train/{}.jpg'.format(filename))
-            feature = self.__vgg16_predict(feature)
-            features.append(feature)
-        return features, labels
 
     def __save_classifier(self):
         fp = open(CLASSIFIER_MODEL, 'wb')
@@ -60,27 +43,53 @@ class ImageMatch(object):
         fp = open(CLASSIFIER_MODEL, 'rb')
         self.__classifier = pickle.load(fp)
 
+    def __save_decomposition(self):
+        fp = open(DECOMPOSITION_MODEL, 'wb')
+        pickle.dump(self.__decomposition, fp)
+
+    def __load_decomposition(self):
+        fp = open(DECOMPOSITION_MODEL, 'rb')
+        self.__decomposition = pickle.load(fp)
+
+    def __vgg16_predict(self, image):
+        image = imresize(image, (224,224)).astype(np.float32)
+        image = imagenet_utils.preprocess_input(image)
+        image = np.reshape(image, (1,224,224,3))
+        label = self.__model.predict(image)
+        return label.ravel()
+
+    def __generate_training_data(self):
+        features, labels = [], []
+        dirlist = os.listdir('data/train')
+        images  = [f for f in dirlist if 'jpg' in f] 
+        for image in images:
+            filename = os.path.splitext(image)[0]
+            label = ''.join(i for i in filename if not i.isdigit())
+            labels.append(label)
+            feature = imread('data/train/{}.jpg'.format(filename))
+            feature = self.__vgg16_predict(feature)
+            features.append(feature)
+        return features, labels
+
     def __train_classifier(self):
+        print('Training...')
         features, labels = self.__generate_training_data()
-        count = len(np.unique(labels))
-        self.__classifier = KNeighborsClassifier(n_neighbors=count)
+        self.__decomposition = PCA()
+        self.__classifier    = LinearSVC()
+        self.__decomposition.fit(features)
+        features = self.__decomposition.transform(features)
         self.__classifier.fit(features, labels)
-        self.__save_classifier()
    
     def __detect_face(self, image):
-        gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
-        faces = self.__cascade.detectMultiScale(
-            gray, 
-            scaleFactor  = 1.1, 
-            minNeighbors = 5
-        )
+        gray  = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
+        faces = self.__cascade.detectMultiScale(gray)
         x,y,w,h = faces[0]
-        image = image[y:y+h,x:x+w]
-        return image
+        return image[y:y+h,x:x+w]
 
     def match(self, image):
         image   = self.__detect_face(image) 
         feature = self.__vgg16_predict(image)
-        label   = self.__classifier.predict([feature])
+        feature = self.__decomposition.transform([feature])
+        label   = self.__classifier.predict(feature)
         return label[0]
 
